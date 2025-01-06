d1fba5060b8eca41fb2b4c36d445eb9b9e580ca1
francoislehmanngrimaldi <francois.lehmann-grimaldi@edu.esiee.fr>
2025-01-06 12:03:49 +0100

    Update variables.tf

--- td3/tofu/modules/alb/variables.tf
+++ td3/tofu/modules/alb/variables.tf
@@ -1,1 +1,19 @@
+variable "name" {
+  description = "Name of the ALB"
+  type        = string
+}
 
+variable "alb_http_port" {
+  description = "Port on which the ALB will listen"
+  type        = number
+}
+
+variable "app_http_port" {
+  description = "Port of the application running on the EC2 instances"
+  type        = number
+}
+
+variable "app_health_check_path" {
+  description = "Path for the ALB health check"
+  type        = string
+}
