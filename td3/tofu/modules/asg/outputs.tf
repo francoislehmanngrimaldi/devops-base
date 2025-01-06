76f34b01b7400002521483603d91493358773396
francoislehmanngrimaldi <francois.lehmann-grimaldi@edu.esiee.fr>
2025-01-06 12:48:43 +0100

    Update outputs.tf

--- td3/tofu/modules/asg/outputs.tf
+++ td3/tofu/modules/asg/outputs.tf
@@ -1,8 +1,7 @@
-
-
 output "security_group_id" {
   description = "The ID of the security group"
-  value = aws_security_group.sample_app[0].id  # Accéder au groupe de sécurité de la première instance
+  value = aws_security_group.sample_app.id
+# Accéder au groupe de sécurité de la première instance
 }
 
 
